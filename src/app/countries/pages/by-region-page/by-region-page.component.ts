2e2314f21ffaed21fd1d0186d2f669c856252dc5
Diego Celaya <diegocelajah@gmail.com>
2024-03-09 22:46:59 -0700

    first and last commit

---
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../services/countries.service';
+
+@Component({
+  selector: 'app-by-region-page',
+  templateUrl: './by-region-page.component.html',
+  styles: [
+  ]
+})
+export class ByRegionPageComponent {
+
+  public countries: Country[] = [];
+  public isLoading: boolean = false;
+
+
+  constructor( private countriesService: CountriesService ){}
+
+  searchByRegion(region: string):void{
+
+    this.isLoading = true;
+
+    this.countriesService.searchRegion(region)
+    .subscribe( countries => {
+      this.countries = countries;
+      this.isLoading = false;
+
+    })
+
+  }
+}
